2a247ff85b8fcbaefb29d4857ae4d34ea1946220
c.keates <capucine@MacBook-Pro-Capucine-Keates.local>
2025-09-17 11:50:03 +0200

    [Commit] Modifs Sections Homepage

--- iris/app/components/Features.tsx
+++ iris/app/components/Features.tsx
@@ -10,14 +10,11 @@ const features = [
   export default function Features() {
     return (
       <div className="bg-gray-100">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
           <div>
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Spécifications</h2>
-            <p className="mt-4 text-gray-500">
-              Notre marque collabore avec des marques éthiques et soucieuses de l'environnement. 
-            </p>
   
-            <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+            <dl className="mt-14 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
               {features.map((feature) => (
                 <div key={feature.name} className="border-t border-gray-200 pt-4">
                   <dt className="font-medium text-gray-900">{feature.name}</dt>
@@ -28,23 +25,24 @@ const features = [
           </div>
           <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
             <img
-              alt="B-corp logo"
-              src="https://heconomist.ch/wordpress_9/wp-content/uploads/2021/05/BCorp_1920x1080_with_lockup.jpg"
+              alt="Cactus."
+              src="https://images.unsplash.com/photo-1554631221-f9603e6808be?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               className="rounded-lg bg-gray-100"
             />
             <img
-              alt="Top down view of walnut card tray with embedded magnets and card groove."
+              alt="Veja flag."
               src="https://images.unsplash.com/photo-1646007086592-9fb49bc00125?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               className="rounded-lg bg-gray-100"
             />
             <img
-              alt="Side of walnut card tray with card groove and recessed card area."
+              alt="Peta approved."
               src="https://plantinside.com/cdn/shop/articles/peta-approved-vegan-certification-plant-inside_1024x1024.png?v=1755769417"
               className="rounded-lg bg-gray-100"
             />
+            
             <img
-              alt="Walnut card tray filled with cards and card angled in dedicated groove."
-              src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-feature-03-detail-04.jpg"
+              alt="B-corp logo"
+              src="https://freerangefuture.com/wp-content/uploads/2022/11/Zorali_B-Corp.jpeg"
               className="rounded-lg bg-gray-100"
             />
           </div>
